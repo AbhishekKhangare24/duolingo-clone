32adb051d002ee449a46355c013568072e834dbb
Abhishek Khangare <abhi@gmail.com>
2024-08-24 18:34:00 +0530

    update header.tsx

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
     <header className="h-20 w-full border-b-2 border-slate-200 px-4">
       <div className="lg:max-w-screen-lg bg-red-500 mx-auto flex items-center justify-between h-full">
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
-          <Image />
+          {/* <Image /> */}
         </div>
       </div>
     </header>
