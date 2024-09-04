ef342a2dfd9965a167bf566a8381c643e236d6f1
Abhishek Khangare <abhi@gmail.com>
2024-09-04 08:35:29 +0530

    Updated imports and added absoluteUrl function to utils.ts

---
+++ lib/admin.ts
@@ -0,0 +1,11 @@
+import { auth } from "@clerk/nextjs";
+
+export const getIsAdmin = () => {
+  const { userId } = auth();
+  // @ts-ignore
+  const adminIds = process.env.CLERK_ADMIN_IDS.split(", "); // stored in .env.local file as string separated by comma(,) and space( )
+
+  if (!userId) return false;
+
+  return adminIds.indexOf(userId) !== -1;
+};
