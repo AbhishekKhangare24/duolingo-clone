d544096385cbaedc588e42dd5cecfe0e667504de
Abhishek Khangare <abhi@gmail.com>
2024-09-04 09:02:45 +0530

    Add prod.ts

--- scripts/prod.ts
+++ scripts/prod.ts
@@ -4,9 +4,8 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "@/db/schema";
 
-//@ts-ignore
 const sql = neon(process.env.DATABASE_URL);
-//@ts-ignore
+
 const db = drizzle(sql, { schema });
 
 const main = async () => {
